3129b3da9a74d6095def1a97862890f1c35a3096
dexSamir <samirah-bp215@code.edu.az>
2025-03-21 17:53:43 +0400

    gitignore file

--- Restore/API/Controllers/ProductsController.cs
+++ Restore/API/Controllers/ProductsController.cs
@@ -1,27 +1,26 @@
 using API.DAL;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers
+namespace API.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController(StoreContext _context) : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController(StoreContext _context) : ControllerBase
+    [HttpGet] //https://localhost:5001/api/products
+    public async Task<ActionResult<List<Product>>> GetProducts()
     {
-        [HttpGet]
-        public ActionResult<List<Product>> GetProducts()
-        {
-            return _context.Products.ToList();
-        }
+        return await _context.Products.ToListAsync();
+    }
 
-        [HttpGet("{id}")]
-        public ActionResult<Product> GetProduct(int id)
-        {
-            var product = _context.Products.Find(id); 
+    [HttpGet("{id}")] //api/products/2
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        var product = await _context.Products.FindAsync(id); 
 
-            if(product == null) return NotFound(); 
+        if(product == null) return NotFound(); 
 
-            return product;
-        }
+        return product;
     }
 }
--- Restore/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ Restore/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e2ef4c0cc2d2c56f6c9aa84c2f03ae956aed4b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebdcda27397ec9a5a8bb54e4cc68328c99ff96cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
