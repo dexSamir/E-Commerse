fe8c90649ff146d68f0f2153d255e6e3c0fb00ee
dexSamir <samirah-bp215@code.edu.az>
2025-03-27 15:17:09 +0400

    using RTK query

--- Restore/client/src/features/contact/ContactPage.tsx
+++ Restore/client/src/features/contact/ContactPage.tsx
@@ -1,13 +1,19 @@
-import { useSelector } from "react-redux";
-import { CounterState } from "./counterReduces";
-import { Typography } from "@mui/material";
+import {  decrement, increment } from "./counterReduces";
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { useAppDispatch, useAppSelector } from "../../app/store/store";
 
 export default function ContactPage() {
-  const data = useSelector((state: CounterState) => state.data);
+  const {data} = useAppSelector(state => state.counter );
+  const dispatch = useAppDispatch()
   return (
     <>
       <Typography variant="h2">Contact Page</Typography>
       <Typography variant="body1">The data is: {data}</Typography>
+      <ButtonGroup>
+        <Button onClick={() => dispatch(decrement(1))} color="error">Decrement</Button>
+        <Button onClick={() => dispatch(increment(1))} color="secondary">Increment</Button>
+        <Button onClick={() => dispatch(increment(5))} color="primary">Increment by 5</Button>
+      </ButtonGroup>
     </>
   );
 }
